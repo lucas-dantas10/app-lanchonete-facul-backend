ed0566ae744019d2e3e472eaf6a3dd6b287bb7f5
Lucas Dantas Nogueira <lucas.dantas.nogueira@gmail.com>
2024-06-06 16:49:21 -0300

    feat: add middleware for validate user isAdmin or not

---
+++ app/Http/Middleware/UserIsAdmin.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class UserIsAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (
+            $request->user()->is_admin == false
+            || empty($request->user)
+        ) {
+            return \response('Usuário não tem permissão!', Response::HTTP_FORBIDDEN);
+        }
+
+        return $next($request);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,14 +4,19 @@ use App\Http\Controllers\Authentication\LoginController;
 use App\Http\Controllers\Users\UserCreateController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use \App\Http\Middleware\UserIsAdmin;
 
 Route::prefix('/v1')->group(function () {
     Route::post('/login', LoginController::class)->name('login');
     Route::post('/create/user', UserCreateController::class)->name('user.create');
 
-    Route::get('/user', function (Request $request) {
-        return $request->user();
-    })->middleware('auth:sanctum');
+    // USUARIO ADMIN E LOGADO
+    Route::middleware([UserIsAdmin::class, 'auth:sanctum'])->group(function () {
+       Route::post('/teste');
+    });
+
+    // USUARIO COMUM E LOGADO
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('/teste');
+    });
 });
-
-
