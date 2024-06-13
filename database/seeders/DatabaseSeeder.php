a798e2fedf945e06886d352dc5082177e15c8611
VitorVts <vitorgguimaraes56@gmail.com>
2024-06-13 17:45:52 -0300

    [feat]Criada as factorys

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -29,6 +29,69 @@ class DatabaseSeeder extends Seeder
             'is_admin' => false,
         ]);
 
-        Product::factory()->count(3)->create();
+        Product::factory()->create([
+            'name' => 'Agua Com Gás',
+            'description'=>'Aguá Gaseficada',
+            'price' => 3.50,
+            'image_path' => 'http://localhost:8000/storage/products/aguaC.png',
+
+        ]);
+        Product::factory()->create([
+            'name' => 'Pizza',
+            'description'=>'Pizza de Mussarela',
+            'price' => 8.00,
+            'image_path' => 'http://localhost:8000/storage/products/pizza.png',
+
+        ]);
+        Product::factory()->create([
+            'name' => 'Hamburguer',
+            'description'=>'Hamburguer com pão carne queijo,',
+            'price' => 8.00,
+            'image_path' => 'http://localhost:8000/storage/products/burger.png',
+
+        ]);
+        Product::factory()->create([
+            'name' => 'Combo',
+            'description'=>'Salgado + Suco ',
+            'price' => 9.00,
+            'image_path' => 'http://localhost:8000/storage/products/combo.png',
+
+        ]);
+        Product::factory()->create([
+            'name' => 'Refrigerante',
+            'description'=>'Lata de Refrigerante',
+            'price' => 6.00,
+            'image_path' => 'http://localhost:8000/storage/products/refrig.png',
+
+        ]);
+        Product::factory()->create([
+            'name' => 'sacole',
+            'description'=>'Sacole de diversos sabores',
+            'price' => 5.00,
+            'image_path' => 'http://localhost:8000/storage/products/aguaC.png',
+
+        ]);
+        Product::factory()->create([
+            'name' => 'Água ',
+            'description'=>'Aguá Normal',
+            'price' => 3.00,
+            'image_path' => 'http://localhost:8000/storage/products/aguaC.png',
+
+        ]);
+        Product::factory()->create([
+            'name' => 'Pipoca',
+            'description'=>'Pipoca salgada',
+            'price' => 4.00,
+            'image_path' => 'http://localhost:8000/storage/products/aguaC.png',
+
+        ]);
+        Product::factory()->create([
+            'name' => 'Biscoito de Polvilho',
+            'description'=>'Biscoito de Polvilho Salgado',
+            'price' => 3.00,
+            'image_path' => 'http://localhost:8000/storage/products/aguaC.png',
+
+        ]);
+        
     }
 }
